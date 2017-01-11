1265512aaa25aa3554162de60c4f50307f7cac75
rkumar1209 <ramesh.kumar@realpage.com>
2017-01-12 00:32:24 +0530

    add sever code

---
+++ server/repository/movie.ts
@@ -0,0 +1,16 @@
+﻿
+import {Db, Collection} from 'mongodb';
+import logger = require('winston');
+import {BaseRepository, IBaseRepository} from '../repository/base';
+import {Movie} from '../models/Movie';
+
+export module Repository { 
+
+    export class MovieRepository extends BaseRepository<Movie>
+    {
+        db: Db;
+        constructor(database: Db) {
+            super(database);
+        }
+    } 
+}
---
+++ server/services/movie.ts
@@ -0,0 +1,23 @@
+﻿import {Movie} from '../models/movie';
+import {Repository} from '../repository/movie';
+import logger = require('winston');
+import {EntityService} from '../services/base';
+
+export module Services
+{
+    export interface IMovieService {
+
+    }
+    export class MovieService extends EntityService<Movie> implements IMovieService
+    {
+        repository: Repository.MovieRepository;
+
+        public constructor(repository: Repository.MovieRepository)
+        {
+            super(repository);
+        }
+    }
+}
+
+
+ 

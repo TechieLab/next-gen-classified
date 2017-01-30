89c00c8e9e6c9470fcc8d11fba6c6729da395f21
ssen <sourabh.sen@realpage.com>
2017-01-30 18:56:40 +0530

    vendor

---
+++ src/app/services/vendor.service.ts
@@ -0,0 +1,17 @@
+import 'rxjs/add/operator/map';
+
+import {Injectable, Optional} from '@angular/core';
+import {Http, Headers, Response, RequestOptions, URLSearchParams} from '@angular/http';
+import { Observable }  from 'rxjs/Observable';
+import {IResult} from '../models/result';
+
+@Injectable()
+export class VendorService {
+    url: string;
+    constructor(public http: Http) {}
+   
+    getCity(resp){
+       let url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + resp.coords.latitude +','+resp.coords.longitude +'&sensor=true'; 
+       return this.http.get(url);
+    }
+}

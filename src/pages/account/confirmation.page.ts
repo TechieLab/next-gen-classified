f156247c29dd204b3cc4130e6a8bc0c642264980
ramesh-sharma12 <ramesh.sharma12@gmail.com>
2017-02-17 13:10:28 +0530

    updated message

--- src/pages/account/confirmation.page.ts
+++ src/pages/account/confirmation.page.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'confirmation-page',
-    template: '<div>Your account created successfully. Please activate your account by clicking on activation link send in email.</div>'
+    templateUrl: 'confirmation.html'
 })
 
 export class ConfirmationPage {

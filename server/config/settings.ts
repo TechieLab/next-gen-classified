4ee1674954eda1e88dcf618884845ff0ce082d06
ramesh-sharma12 <ramesh.sharma12@gmail.com>
2017-03-27 01:50:52 +0530

    fixed profile image

--- server/config/settings.ts
+++ server/config/settings.ts
@@ -1,4 +1,4 @@
 export class Settings{
-    public static BackendHost : string = 'http://192.168.0.105:3000/';
+    public static BackendHost : string = 'http://192.168.0.107:3000/';
     public static ImageRepository : string = 'images/'
 }
--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -7,6 +7,6 @@ export class Constants {
   public static LookupApi: string = Constants.BaseApi + '/api/lookups/';
   public static PostApi : string =  Constants.BaseApi +'/api/posts/';
   public static AccountApi: string = Constants.BaseApi + '/api/account/';
-  public static ProfileApi : string = Constants.BaseApi + 'api/profile/';
+  public static ProfileApi : string = Constants.BaseApi + '/api/profile/';
 
 }

7ce6b203a65182bdb169cd49194fe4b3075bfa44
ramesh-sharma12 <ramesh.sharma12@gmail.com>
2017-04-02 22:59:54 +0530

    updated code

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -2,8 +2,8 @@
 export class Constants {
 
   public static AppName: string = "Rp Classified";
-  public static BaseApi: string = 'http://127.0.0.1:3000' /*http://192.168.0.107:3000*/;
-  public static ElasticApi : string = "http://192.168.0.107:9201";
+  public static BaseApi: string = /*'http://127.0.0.1:3000' */'http://ramesh-sharma.cloudapp.net';
+  public static ElasticApi : string = "http://52.187.133.250:9200";
   public static LookupApi: string = Constants.BaseApi + '/api/lookups/';
   public static PostApi : string =  Constants.BaseApi +'/api/posts/';
   public static AccountApi: string = Constants.BaseApi + '/api/account/';

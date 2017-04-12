f503a56a7dade1b837bf452beba0211950dbed78
ramesh-sharma12 <ramesh.sharma12@gmail.com>
2017-04-13 00:43:48 +0530

    fixed upload folder

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -17,11 +17,11 @@ export class IndexRoute {
         });
 
         this.app.get('/images/profiles/:name', function (req, res) {
-            return res.sendFile(path.join(__dirname, '../../../uploads/profiles/' + req.params.name));
+            return res.sendFile(path.join(__dirname, '../uploads/profiles/' + req.params.name));
         });
 
         this.app.get('/images/posts/:id/:name', function (req, res) {
-            return res.sendFile(path.join(__dirname, '../../../uploads/posts/' + req.params.id + '/' + req.params.name));
+            return res.sendFile(path.join(__dirname, '../uploads/posts/' + req.params.id + '/' + req.params.name));
         });
     }
 }
